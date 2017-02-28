b331dc6a4b778499bd0b8932efeab607160ae5fe
Slav Man <anthony.horrar11@yahoo.com>
2017-02-28 15:22:50 -0500

    Fixed via the Test01

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,20 +1,22 @@
+import java.lang.Math;
 public class Recursion{ 
-    public static String name(){return "Kurkin,Anthony"}
+    public static String name(){return "Kurkin,Anthony";}
     public static double sqrt(double n){
-	if(n < 0){
-	    throw IllegalArguementException("You can't do that! MAN!!!");
+	if(n == 0.0){return 0.0;}
+	else if(n <= 0){
+	    throw new IllegalArgumentException("One cannot simply give this type of value for this method...");
 	}
 	else{
 	    return sqrtHelp(n,1);
 	}
     } 
-    public static sqrtHelp(double number, double guess){
-	double better = (n/guess + guess)/2;
-	if((guess - better)/better < .000001){
+    public static double sqrtHelp(double number, double guess){
+	double better = (number/guess + guess)/2;
+	if((Math.abs(guess - better)/better) < .0000000000001){
 	    return better;
 	}
 	else{
-	    return sqrtHelp(n,better);
+	    return sqrtHelp(number,better);
 	}
     }
 }
