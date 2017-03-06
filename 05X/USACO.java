c4340ef38d14dab398f2ad13cb0495742e635588
Slav Man <anthony.horrar11@yahoo.com>
2017-03-06 15:37:05 -0500

    working

---
+++ 05X/USACO.java
@@ -0,0 +1,19 @@
+public class USACO{
+    public int bronze(String filename){
+	private int R,C,E,N;
+	private int[][] lake;
+	File file = new File(filename);
+	Scanner in = new Scanner(file);
+	while(in.hasNextLine()){
+	    String line = in.nextLine();
+	    for(int i = 0; i < 4; i++){
+		String temp = in.next();
+		int x = temp.parseInt();
+		if(i == 0){}//work on this you need to parse ints from ALL lines
+	    }//but the info you need to instatiate is only in first line
+	}//so test for that, and then use the other lines fo rthe other stuff.
+	
+	
+    }
+    public int silver(String filename){}
+}
