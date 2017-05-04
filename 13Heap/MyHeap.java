7150b3752e686bf20db5565df412889b89e97b94
Slav Man <anthony.horrar11@yahoo.com>
2017-05-04 15:34:21 -0400

    up-date

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,9 +1,33 @@
+import java.util.*;
 public class MyHeap{
-    public MyHeap(){}
+    
+    private ArrayList<String> heap;
+    public MyHeap(){
+	heap = new ArrayList<String>(1);
+	heap.add("DUMMY");
+    }
+    
     public MyHeap(boolean){}
-    public void add(String s){}
-    public String remove(){}
+    
+    public void add(String s){
+	heap.add(s);
+	
+	
+    }
+
+    public String remove(int index){
+	String toRet = heap.remove(index);
+	heap.add(index, heap.get(heap.size() - 1));
+	heap.remove(heap.size()-1);
+	heap.trimToSize(heap.size()-1);
+	
+
+	return toRet;
+    }
+
     public String peek(){}
+
     public pushUp(){}
+
     public pushDown(){}
 }
